6bfb4a8a744b779cdf782f2adc5219b70cbb6e04
BallArat55 <153797238+BallArat55@users.noreply.github.com>
2024-01-03 00:17:59 +0100

    Update few.js

--- few.js
+++ few.js
@@ -4,6 +4,7 @@ a ≠ 0
 
 const xArray = [50,60,70,80,90,100,110,120,130,140,150];
 const yArray = [7,8,8,9,9,9,10,11,14,14,15];
+const yArray2 = [7,8,8,9,9,9,10,11,14,14,15];
 
 // Plot Scatter
 ctx.fillStyle = "red";
