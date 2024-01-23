7e3837c8fb87ca9cffccbe910bf97c98388a5285
BallArat55 <153797238+BallArat55@users.noreply.github.com>
2024-01-23 18:23:41 +0100

    Update few.js

--- few.js
+++ few.js
@@ -3,7 +3,7 @@ a, b and c are real numbers and
 a ≠ 0
 
 const xArray = [50,60,70,100];
-const yArray = [7,8,8,9,9,9,14,14,15];
+const yArray = [7,8,8,9,9,9,14,15];
 const yArray2 = [7,8,8,9,9,9,102,1,14,15,19];
 
 // Plot Scatter
