50c46c626fe13a5f322bd4fa11d826a5126064e3
BallArat55 <153797238+BallArat55@users.noreply.github.com>
2024-01-27 17:13:51 +0100

    Update few.js

--- few.js
+++ few.js
@@ -3,8 +3,8 @@ a, b and c are real numbers and
 a ≠ 0
 
 const xArray = [50,60,70,100];
-const yArray = [7,8,8,9,9,9,14,15];
-const yArray2 = [7,8,8,9,9,9,0,1,14,19];
+const yArray = [7,8,8,9,9,14,15];
+const yArray2 = [7,8,8,9,9,0,1,14,19];
 
 // Plot Scatter
 ctx.fillStyle = "red";
