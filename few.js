dffef6988ca8a5b9514f4648c3e44acf26e925f2
BallArat55 <153797238+BallArat55@users.noreply.github.com>
2024-02-27 18:09:25 +0100

    Update few.js

--- few.js
+++ few.js
@@ -8,7 +8,7 @@ const yArray2 = [7,9,19];
 // Plot Scatter
 ctx.fillStyle = "red";
 for (let i = 0; i < xArray.length-1; i+) {
-  let x = xArray[i]*40/150;
+  let x = xArray[i]*0/150;
   let y = yArray[i]*400/15;
   ctx.beginPath();
   ctx.ellipse(x, y, 2, 3, 0, 0, Math.PI * 2);
